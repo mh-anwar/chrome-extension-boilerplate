9e9faf46cc83103d2a1af3d95f9c9c033a429bbf
mh-anwar <mohammad.anwar2939@gmail.com>
2021-05-09 16:39:12 -0400

    Popup added

--- background-script.js
+++ background-script.js
@@ -1,3 +1,4 @@
+
 var title = "YAYAYAYAYAY";
 var today = new Date();
 
@@ -8,12 +9,13 @@ var min = today.getMinutes()
 var time = hrs + ' ' + min
 var sec = today.getSeconds();
 var dateTime = date
-if (hrs == "20") {
+if (sec = "50") {
   browser.notifications.create({
     "type": "basic",
-    "iconUrl": browser.extension.getURL("icons/link-48.png"),
+    "iconUrl": browser.extension.getURL("images/icon32.jpeg"),
     "title": title,
     "message": dateTime
   });
+  console.log("WOIOOO")
+  console.log(min)
 }
-console.log(min)
